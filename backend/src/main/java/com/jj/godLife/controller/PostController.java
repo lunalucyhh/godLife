f06f80c96b7bb9bfa1d71e66545e03b0178e618c
TASK\luna7 <rkrdh727@gmail.com>
2023-11-12 21:25:03 +0900

    초기세팅

--- backend/src/main/java/com/jj/godLife/controller/PostController.java
+++ backend/src/main/java/com/jj/godLife/controller/PostController.java
@@ -1,5 +1,17 @@
 package com.jj.godLife.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/post")
 public class PostController {
-	
+
+    @GetMapping
+    public ResponseEntity<String> get()
+    {
+        return ResponseEntity.ok("ok");
+    }
 }
