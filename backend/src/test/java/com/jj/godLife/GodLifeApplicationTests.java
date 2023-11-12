695ab72d1505d01deda6fb3255443bbf81fe3630
Lucy Bugwang <hellolucy1010@gmail.com>
2023-11-12 17:09:09 +0900

    Backend Source Code Init

---
+++ backend/src/test/java/com/jj/godLife/GodLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jj.godLife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
