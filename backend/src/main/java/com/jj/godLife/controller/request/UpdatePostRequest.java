4ca54eba83a02459a4f7f807c56f4b9dd6ef198c
TASK\luna7 <rkrdh727@gmail.com>
2023-11-19 21:28:04 +0900

    ToString추가

--- backend/src/main/java/com/jj/godLife/controller/request/UpdatePostRequest.java
+++ backend/src/main/java/com/jj/godLife/controller/request/UpdatePostRequest.java
@@ -1,7 +1,9 @@
 package com.jj.godLife.controller.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.ToString;
 
+@ToString
 public class UpdatePostRequest {
     @JsonProperty("board_no")//들어옴
     private Long boardNo;//받음
