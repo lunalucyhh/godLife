eaa8d09189ec19e1093b27b13b81c34defceb780
TASK\luna7 <rkrdh727@gmail.com>
2024-03-21 21:13:30 +0900

    프론트 엔드 페이지 만들기

---
+++ frontend2/src/pages/Main.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+const Wrapper = styled.div`
+    padding: 16px;
+    width: calc(100% - 32px);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+`;
+
+const Container = styled.div`
+    width: 100%;
+    max-width:720px;
+
+    & > * {
+        :not(:last-child) {
+            margin-bottom: 16px;
+        }
+    }
+
+`;
+
+function MainPage() {
+
+    return(
+        <Wrapper>
+                <p>갓생 살기</p>
+            <Container>
+                <p>당신의 순간을 응원합니다</p>    
+            </Container>
+        </Wrapper>
+    );
+}
+
+export default MainPage;
